8a5a3ddf8816068dc35d8005b036ef6a18bdf0b6
kanbaknurullah <kanbaknurullah@gmail.com>
2022-05-05 15:36:50 +0300

    First commit

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'disagree must voice surface broom flash else motor item powder lake alpha',
+  'https://rinkeby.infura.io/v3/1a7fe401ed444925a0cd096f0f80ccf3'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
